69b2dcff4972b7f19230a61e7f0d4d7b1d692e66
Maxmala <37975336+Maxmala@users.noreply.github.com>
2018-04-10 15:43:03 +0200

    Add files via upload

--- 02_salade_cesar/02_salade_cesar.rb
+++ 02_salade_cesar/02_salade_cesar.rb
@@ -1,9 +1,9 @@
 Alphabet = 26
 def chiffre_de_cesar(string, shift = 1)
-    alphabet   = Array('a'..'z')
-    encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
-    string.chars.map { |c| encrypter.fetch(c, " ") }
-  end
+  alphabet   = Array('a'..'z')
+  encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  string.chars.map { |c| encrypter.fetch(c, " ") }
+end
 
 puts chiffre_de_cesar("salade").join
-print chiffre_de_cesar("What a string!", 5).join
+print chiffre_de_cesar("what a string", 5).join
